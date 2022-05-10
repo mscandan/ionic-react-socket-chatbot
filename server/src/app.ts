ad4152fc34eaac44b81410403947d16aad4cf161
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2022-05-11 01:01:01 +0300

    feat: add sending message functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,36 @@
 /* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-const server = app.listen(3000, () => {
-  console.log(`server started on port 3000}`);
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
 
-app.get('/', (_req, res) => {
-  res.status(200).json({
-    message: 'works',
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8100',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', socket => {
+  console.log('user connected ', socket.id);
+
+  socket.on('send-message', (data: { message: string; sender: string; time: Date }) => {
+    console.log('user sent a message ', data);
+    socket.emit('receive-message', 'burdasin');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected', socket.id);
   });
 });
 
